ce276be041bf77fe549ccc118a0b14e89484b6ec
unknown <alberto.mercati@gmail.com>
2015-01-26 22:30:01 +0100

    enabled gravitational interaction with 2 bodies; added diagnostic logs

--- WebContent/script/main3d.js
+++ WebContent/script/main3d.js
@@ -1,11 +1,13 @@
 Constants = {
 		"G" : 6.673E-11,
-		"EARTH_MASS" : 	5.972E+24,
-		"MOON_MASS" : 7.348E+22,
+		"EARTH_MASS" : 10E13, // 	5.972E+24,
+		"MOON_MASS" : 1E2, // 7.348E+22,
 		"JUPITER_MASS" : 1.8986E+27,
 		"EARTH_MOON_DISTANCE" : 384400000, //384,400 Km
 		"EARTH_MOON_SCREEN_DISTANCE" : 50,
-		"DISTANCE_SCALE_FACTOR" :  undefined
+		"DISTANCE_SCALE_FACTOR" :  undefined,
+		
+		"LOG_ENABLED" : true
 }
 
 Constants.DISTANCE_SCALE_FACTOR = Constants.EARTH_MOON_DISTANCE / Constants.EARTH_MOON_SCREEN_DISTANCE;
@@ -15,7 +17,7 @@ function threeApp()
 	var scene = new THREE.Scene();
 	var width = 40;
 	
-//	var height = 40 / ( window.innerWidth / window.innerHeight);
+//	var height = width / ( window.innerWidth / window.innerHeight);
 //	var camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 ); 
 	var camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 1000);
 	
@@ -67,24 +69,26 @@ function threeApp()
     var planetGeometry = new THREE.SphereGeometry( .5, 64, 64);
     material = new THREE.MeshPhongMaterial( {color: 0xffff00} );
     planetSphere = new THREE.Mesh( planetGeometry, material );
+    planet = new CelestialBody(Constants.EARTH_MASS, new THREE.Vector3(0, 0, 0), planetSphere);
     scene.add( planetSphere );
 
     var moonGeometry = new THREE.SphereGeometry( .5, 64, 64);
     material = new THREE.MeshPhongMaterial( {color: 0xff0000} );
     moonSphere = new THREE.Mesh( moonGeometry, material );
+    moon = new CelestialBody(Constants.MOON_MASS, new THREE.Vector3(0, 15, 3), moonSphere);
     scene.add( moonSphere );
     
     //
 	drawAxis(scene);
     //
     
-    moonSphere.position.x = 25;
+    moonSphere.position.x = 20;
     moonSphere.position.y = 0;
-    moonSphere.position.z = -4.5;
+    moonSphere.position.z = 0;
     
-	camera.position.z = 25;
+	camera.position.z = 30;
 	camera.position.x = 2;
-	camera.position.y = 4;
+	camera.position.y = 10;
 //	camera.rotation.x = - Math.PI / 8;
 //	camera.rotation.y = - Math.PI / 8;
 //	camera.rotation.z = - Math.PI / 24;
@@ -119,12 +123,20 @@ function threeApp()
 		
 //		console.log(moonX + ", " + moonY);
 		
-		moonSphere.position.x = moonX;
-		moonSphere.position.y = moonY;
-		moonSphere.position.z = moonZ;
-        
-		controls.update();
+		// old: a stable periodic motion along an impossible trajectory 
+//		moonSphere.position.x = moonX;
+//		moonSphere.position.y = moonY;
+//		moonSphere.position.z = moonZ;
+//		moonSphere.position = new THREE.Vector3(moonX, moonY, moonZ);
 		
+		// new: calculates gravitational force
+		moon.addForceContribution(planet);
+		planet.addForceContribution(moon);
+		
+		moon.updatePosition(delta);
+		planet.updatePosition(delta);
+		
+		controls.update();
 		renderer.render(scene, camera);
 	}
 	
@@ -179,7 +191,10 @@ function CelestialBody(mass, velocity, mesh){
 	
 	this.squareDistanceFrom = function(body){
 		myPosition = this.getPosition();
+		customLog("myPosition = " + myPosition);
 		var squareDistance = body.getPosition().distanceToSquared(myPosition);
+		customLog("squareDistance = " + squareDistance);
+		return squareDistance;
 	};
 	
 	this.distanceFrom = function(body){
@@ -188,14 +203,17 @@ function CelestialBody(mass, velocity, mesh){
 	
 	this.forceBetween = function(body){
 		var squareDistance = this.squareDistanceFrom(body);
-		var force = G * (this.mass * body.mass) / squareDistance;
-		return -1 * force;
+		customLog("mass1 = " + this.mass + "; mass2 = " + body.mass + "; product = " + this.mass * body.mass);
+		var force = Constants.G * (this.mass * body.mass) / squareDistance;
+		customLog("force = " + force);
+		return force;
 	};
 	
 	this.addForceContribution = function(body){
 		var forceMagnitude = this.forceBetween(body);
+		customLog("forceMagnitude = " + forceMagnitude);
 		var distance = this.distanceFrom(body);
-		
+		customLog("distance = " + distance);
 		var xDiff = body.getPosition().x - this.getPosition().x;
 		var yDiff = body.getPosition().y - this.getPosition().y;
 		var zDiff = body.getPosition().z - this.getPosition().z;
@@ -210,17 +228,31 @@ function CelestialBody(mass, velocity, mesh){
 		
 		var forceVector = new THREE.Vector3(fx, fy, fz);
 		
-		this.acceleration = this.acceleration.add( forceVector.divideScalar(this.mass) );
+		this.acceleration = this.acceleration.add(forceVector.divideScalar(this.mass));
 	};
 	
 	this.updatePosition = function(delta){
+		// delta = .04; // just to make debugging possible
+		customLog("velocity before update = " + JSON.stringify(this.velocity));
 		this.velocity = this.velocity.add(this.acceleration.multiplyScalar(delta));
+		customLog("velocity after update = " + JSON.stringify(this.velocity));
+		var nextPosition = this.getPosition().add( this.velocity.clone().multiplyScalar(delta) );
 		
-		this.mesh.position = this.getPosition().add( this.velocity.multiplyScalar(delta) );
+		this.mesh.position.x = nextPosition.x;
+		this.mesh.position.y = nextPosition.y;
+		this.mesh.position.z = nextPosition.z;
 		
-		this.acceleration = new THREE.Vector3(0, 0, 0);
+		// customLog("position = " + JSON.stringify( this.mesh.position ));
+		//this.acceleration = new THREE.Vector3(0, 0, 0);
+		customLog("velocity after acceleration update = " + JSON.stringify(this.velocity));
 	};
 }
+
+function customLog(message){
+	if(Constants.LOG_ENABLED){
+		console.log(message);
+	}
+} 
 
 
 
