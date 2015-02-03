654503f488cff35719320ffc2f1ca5987619335d
unknown <alberto.mercati@gmail.com>
2015-02-03 21:32:31 +0100

    added GPL3 license

--- WebContent/script/CelestialBody.js
+++ WebContent/script/CelestialBody.js
@@ -1,5 +1,20 @@
 /**
- * 
+   Copyright (c) 2015 Alberto Mercati
+ 
+    This file is part of Gravity.
+
+    Gravity is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gravity is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gravity. If not, see <http://www.gnu.org/licenses/>.
  */
 
 function CelestialBody(mass, velocity, mesh){
--- WebContent/script/meshAddition.js
+++ WebContent/script/meshAddition.js
@@ -1,7 +1,18 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 Alberto Mercati
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 function addOriginMassiveBody(scene){
@@ -51,7 +62,7 @@ function addSpiralOfBodies(scene){
         var distance = 40 + i * 2;
         
         var x  = distance * Math.cos(alpha * i);
-        var y = .5 * i;
+        var y = .2 * i;
         var z = distance * Math.sin(alpha * i);
         
         var vConst = 5;
